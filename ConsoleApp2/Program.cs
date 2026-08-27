b0658ae2081b156179fd77fe399d262fbcc5684a
d3n1zws <danizeh@code.edu.az>
2026-08-27 17:07:04 +0400

    added task2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("color:");
+string color = Console.ReadLine();
+Console.WriteLine("year:");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine("brand:");
+string brand = Console.ReadLine();
+Console.WriteLine("model:");
+string model = Console.ReadLine();
+Console.WriteLine("fuelCapacity:");
+double fuelCapacity = double.Parse(Console.ReadLine());
+Console.WriteLine("fuelFor1Km:");
+double fuelFor1Km = double.Parse(Console.ReadLine());
+Console.WriteLine("currentFuel:");
+double currentFuel = double.Parse(Console.ReadLine());
+
+Vechile vechile1 = new Vechile(color, year, brand, model, fuelCapacity, fuelFor1Km, currentFuel);
+vechile1.ShowInfo();
+vechile1.Drive(100.0);
---
+++ ConsoleApp2/Vechile.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+
+class Vechile
+{
+    public string Color;
+    public int Year;
+    public string Brand;
+    public string Model;
+    public double FuelCapacity; // max yanacaq tutumu
+    public double FuelFor1Km; // 1km yanacaq
+    public double CurrentFuel; // yanacaq 
+    public Vechile(
+        string color,
+        int year,
+        string brand,
+        string model,
+        double fuelCapacity,
+        double fuelFor1Km,
+        double currentFuel)
+    {
+        Color = color;
+        Year = year;
+        Brand = brand;
+        Model = model;
+        FuelCapacity = fuelCapacity;
+        FuelFor1Km =  fuelFor1Km;
+        CurrentFuel = currentFuel;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine(Color);
+        Console.WriteLine(Year);
+        Console.WriteLine(Brand);
+        Console.WriteLine(Model);
+        Console.WriteLine(FuelCapacity);
+        Console.WriteLine(FuelFor1Km);
+        Console.WriteLine(CurrentFuel);
+    }
+    public void Drive(double km)
+    {
+        if (km * FuelFor1Km <= CurrentFuel)
+        {
+            CurrentFuel -= km * FuelFor1Km;
+            Console.WriteLine($"qalan yanacaq : {CurrentFuel}");
+        }
+        else
+        {
+            Console.WriteLine("hemin mesafeni gede bilmir.");
+        }
+    }
+}
