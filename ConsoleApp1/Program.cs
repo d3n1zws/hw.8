65e3a1e646420ebb0c6e91e48783488a075a5d4c
d3n1zws <danizeh@code.edu.az>
2026-08-27 13:50:23 +0400

    task1 added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿Account acc = new();
+acc.username = "lorem123";
+acc.password = "123456";
+
+string Username = Console.ReadLine();
+string Password = Console.ReadLine();
+acc.login(Username, Password);
